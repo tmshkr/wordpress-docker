26f0ce0ce83b2f300f0f8e275e15ea2d448531ab
Tim Shaker <tim@tmshkr.com>
2025-01-18 12:19:20 -0800

    add deploy config

---
+++ scripts/deployment-prep.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+if [[ -z $MYSQL_ROOT_PASSWORD ]]; then
+    echo "MYSQL_ROOT_PASSWORD is required"
+    exit 1
+fi
+
+if [[ -z $MYSQL_PASSWORD ]]; then
+    echo "MYSQL_PASSWORD is required"
+    exit 1
+fi
+
+echo "MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD" >>.env
+echo "MYSQL_DATABASE=wordpress" >>.env
+echo "MYSQL_USER=wordpress" >>.env
+echo "MYSQL_PASSWORD=$MYSQL_PASSWORD" >>.env
+echo "WORDPRESS_DB_HOST=db" >>.env
+echo "WORDPRESS_DB_USER=wordpress" >>.env
+echo "WORDPRESS_DB_PASSWORD=$MYSQL_PASSWORD" >>.env
+echo "WORDPRESS_DB_NAME=wordpress" >>.env
+
+zip -r bundle.zip . -x '*.git*'

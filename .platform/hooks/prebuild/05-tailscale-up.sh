6d0edf16de5354efedd21031edc0b56b0d3fa6e1
Tim Shaker <tim@tmshkr.com>
2025-01-18 12:45:05 -0800

    add TAILSCALE_AUTH_KEY

--- .platform/hooks/prebuild/05-tailscale-up.sh
+++ .platform/hooks/prebuild/05-tailscale-up.sh
@@ -3,4 +3,6 @@
 set -o xtrace
 set -e
 
-curl -fsSL https://tailscale.com/install.sh | sh && sudo tailscale up --auth-key=
+source .env
+
+curl -fsSL https://tailscale.com/install.sh | sh && sudo tailscale up --auth-key=$TAILSCALE_AUTH_KEY

ae32b21d819b1c48fdfff8e47a5226841987a2a4
Tim Shaker <tim@tmshkr.com>
2025-01-19 10:33:28 -0800

    add caddy config

--- scripts/deployment-prep.sh
+++ scripts/deployment-prep.sh
@@ -2,11 +2,11 @@
 
 # Create an array of required environment variables
 required_env_vars=(
-    "HTTPS_CERTIFICATE"
-    "HTTPS_PRIVATE_KEY"
     "MYSQL_ROOT_PASSWORD"
     "MYSQL_PASSWORD"
+    "SERVER_NAME"
     "TAILSCALE_AUTH_KEY"
+    "TLS_AUTO_EMAIL"
 )
 
 # Iterate over the array of required environment variables
@@ -20,9 +20,10 @@ for env_var in "${required_env_vars[@]}"; do
     fi
 done
 
-# nginx
-echo "$HTTPS_CERTIFICATE" >ssl/certificate.crt
-echo "$HTTPS_PRIVATE_KEY" >ssl/private.key
+# Caddy
+echo "SERVER_NAME=$SERVER_NAME" >>caddy/.env
+echo "TLS_MODE=tls_auto" >>caddy/.env
+echo "TLS_AUTO_EMAIL=$TLS_AUTO_EMAIL" >>caddy/.env
 
 # MariaDB
 echo "MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD" >>.env
@@ -32,11 +33,5 @@ echo "MYSQL_PASSWORD=$MYSQL_PASSWORD" >>.env
 
 # Tailscale
 echo "TAILSCALE_AUTH_KEY=$TAILSCALE_AUTH_KEY" >>.env
-
-# Wordpress
-echo "WORDPRESS_DB_HOST=db" >>.env
-echo "WORDPRESS_DB_USER=wordpress" >>.env
-echo "WORDPRESS_DB_PASSWORD=$MYSQL_PASSWORD" >>.env
-echo "WORDPRESS_DB_NAME=wordpress" >>.env
 
 zip -r bundle.zip . -x '*.git*'
--- ssl/dev-cert.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-openssl genrsa -out private.key 2048
-openssl req -new -key private.key -out certificate.csr
-openssl x509 -req -days 365 -in certificate.csr -signkey private.key -out certificate.crt

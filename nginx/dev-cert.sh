7668a594039593e37720c063e7214f293b474640
Tim Shaker <tim@tmshkr.com>
2025-01-18 18:03:20 -0800

    add https config

---
+++ nginx/dev-cert.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+
+openssl genrsa -out private.key 2048
+openssl req -new -key private.key -out certificate.csr
+openssl x509 -req -days 365 -in certificate.csr -signkey private.key -out certificate.crt
--- scripts/deployment-prep.sh
+++ scripts/deployment-prep.sh
@@ -1,25 +1,41 @@
 #!/bin/bash -e
 
-if [[ -z $MYSQL_ROOT_PASSWORD ]]; then
-    echo "MYSQL_ROOT_PASSWORD is required"
-    exit 1
-fi
+# Create an array of required environment variables
+required_env_vars=(
+    "HTTPS_CERTIFICATE"
+    "HTTPS_PRIVATE_KEY"
+    "SERVER_NAME"
+    "MYSQL_ROOT_PASSWORD"
+    "MYSQL_PASSWORD"
+    "TAILSCALE_AUTH_KEY"
+)
 
-if [[ -z $MYSQL_PASSWORD ]]; then
-    echo "MYSQL_PASSWORD is required"
-    exit 1
-fi
+# Iterate over the array of required environment variables
+for env_var in "${required_env_vars[@]}"; do
+    # Check if the environment variable is not set
+    if [[ -z "${!env_var}" ]]; then
+        # Print an error message
+        echo "$env_var is required"
+        # Exit the script with a non-zero status
+        exit 1
+    fi
+done
 
-if [[ -z $TAILSCALE_AUTH_KEY ]]; then
-    echo "TAILSCALE_AUTH_KEY is required"
-    exit 1
-fi
+# nginx
+echo "HTTPS_CERTIFICATE=$HTTPS_CERTIFICATE" >>nginx/certificate.crt
+echo "HTTPS_PRIVATE_KEY=$HTTPS_PRIVATE_KEY" >>nginx/private.key
+echo "SERVER_NAME=$SERVER_NAME" >>.env
 
+# MariaDB
 echo "MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD" >>.env
 echo "MYSQL_DATABASE=wordpress" >>.env
 echo "MYSQL_USER=wordpress" >>.env
 echo "MYSQL_PASSWORD=$MYSQL_PASSWORD" >>.env
+
+# Tailscale
 echo "TAILSCALE_AUTH_KEY=$TAILSCALE_AUTH_KEY" >>.env
+
+# Wordpress
 echo "WORDPRESS_DB_HOST=db" >>.env
 echo "WORDPRESS_DB_USER=wordpress" >>.env
 echo "WORDPRESS_DB_PASSWORD=$MYSQL_PASSWORD" >>.env

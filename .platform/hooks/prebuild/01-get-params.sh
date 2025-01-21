1cd2715ac5f9230c0e8d43500fa04ff62ce5c6f8
Tim Shaker <tim@tmshkr.com>
2025-01-20 16:12:13 -0800

    add 01-get-params.sh

---
+++ .platform/hooks/prebuild/01-get-params.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -o xtrace
+set -e
+
+source .env
+# Retrieve parameters from AWS SSM
+repo="${GITHUB_REPOSITORY#*/}"
+aws ssm get-parameters-by-path --path "/$repo/$STAGE/" \
+    --recursive --with-decryption --query "Parameters[*].[Name,Value]" \
+    --output text | while read -r name value; do
+    key=$(echo $name | sed "s/\/$repo\/$STAGE\///")
+    echo "$key=$value" >>.env
+done
+
+aws ssm get-parameter --name "/$repo/DEPLOY_KEY" \
+    --with-decryption --query "Parameter.Value" --output text >~/.ssh/id_ed25519

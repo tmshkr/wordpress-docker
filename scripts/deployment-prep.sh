0874a9b9cb3d6dd443aca4a9ac7d356f6f83769d
Tim Shaker <tim@tmshkr.com>
2025-01-20 16:16:05 -0800

    update deploy

--- scripts/deployment-prep.sh
+++ scripts/deployment-prep.sh
@@ -2,14 +2,10 @@
 
 # Create an array of required environment variables
 required_env_vars=(
-    "EB_DEPLOY_PRIVATE_KEY"
     "GITHUB_ACTOR"
     "GITHUB_REPOSITORY"
     "GITHUB_SHA"
-    "MYSQL_ROOT_PASSWORD"
-    "MYSQL_PASSWORD"
     "SERVER_NAME"
-    "TAILSCALE_AUTH_KEY"
     "TLS_AUTO_EMAIL"
 )
 
@@ -30,18 +26,12 @@ echo "TLS_MODE=tls_auto" >>caddy/.env
 echo "TLS_AUTO_EMAIL=$TLS_AUTO_EMAIL" >>caddy/.env
 
 # GitHub
-echo $EB_DEPLOY_PRIVATE_KEY >id_ed25519
 echo "GITHUB_ACTOR=$GITHUB_ACTOR" >>.env
 echo "GITHUB_REPOSITORY=$GITHUB_REPOSITORY" >>.env
 echo "GITHUB_SHA=$GITHUB_SHA" >>.env
 
 # MariaDB
-echo "MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD" >>.env
 echo "MYSQL_DATABASE=wordpress" >>.env
 echo "MYSQL_USER=wordpress" >>.env
-echo "MYSQL_PASSWORD=$MYSQL_PASSWORD" >>.env
-
-# Tailscale
-echo "TAILSCALE_AUTH_KEY=$TAILSCALE_AUTH_KEY" >>.env
 
 zip -r bundle.zip . -x '*.git*'
